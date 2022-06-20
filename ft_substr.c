8e1143519d9e0b718f1b8d93bce71deff47df774
Mahmoud Khalil <m94khalil@gmail.com>
2022-06-20 11:25:24 +0400

    substr

---
+++ ft_substr.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <libft.h>
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char		*sub;
+	unsigned int	i;
+
+	sub = malloc(len * sizeof(char));
+	if (sub == 0)
+		return (0);
+	i = 0;
+	while (i < len)
+	{
+		sub[i] = s[start + i];
+		i++;
+	}
+	return (sub);
+}
