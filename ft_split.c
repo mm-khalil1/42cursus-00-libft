c195f0c0daa2c4f9821afe592190ef3b8db29f44
Mahmoud Khalil <m94khalil@gmail.com>
2022-06-21 16:21:58 +0400

    split finally

--- ft_split.c
+++ ft_split.c
@@ -1,5 +1,4 @@
 #include <libft.h>
-#include <stdlib.h>
 
 int	count_words(const char *str, char c)
 {
@@ -12,13 +11,13 @@ int	count_words(const char *str, char c)
 	while (str[i])
 	{
 		dil = ft_strchr(str + i, c);
-		if (!dil && i < ft_strlen(str) - 1){
+		if (!dil){
 			count++;
 			i = ft_strlen(str) - 1;
 		}
 		else {
 			i = (int) (dil - str);
-			if (dil && i > 0 && str[i - 1] != 0) 
+			if (dil && i > 0 && str[i - 1] != c) 
 				count++;
 		}
 		i++;
@@ -26,28 +25,39 @@ int	count_words(const char *str, char c)
 	return (count);
 }
 
-char	*isword(const char *s, char c)
+int	next_chr(const char *s, char c, size_t i)
 {
-
-
+	while (s[i] == c && s[i])
+		i++;
+	if (!s[i])
+		return (i - 1);
+	return (i);
 }
 
 char	**ft_split(char const *s, char c)
 {
-	int	i;
+	size_t	i;
+	size_t	j;
+	int	first_ind;
 	char	**split;
-	int	words_count;
 
-	if (!s || !c)
-		return (0);
-	words_count = count_words(s, c);
-	split = malloc(sizeof(char) * (words_count + 1));
-	if (!split)
+	split = malloc(sizeof(char) * (count_words(s, c) + 1));
+	if (!s || !c || !split)
 		return (0);
 	i = 0;
-	while (i < words_count){
-		
+	j = 0;
+	first_ind = -1;
+	while (i <= ft_strlen(s)){
+		if (s[i] != c && first_ind == -1)
+			first_ind = i;
+		if ((s[i] == c || s[i] == 0) && first_ind > -1)
+		{
+			split[j++] = ft_substr(s, first_ind, i - first_ind);
+			i = next_chr(s, c, i + 1);
+			first_ind = i;
+		}
+		i++;
 	}
-	split[i] = 0;
+	split[j] = 0;
 	return (split);
 }
