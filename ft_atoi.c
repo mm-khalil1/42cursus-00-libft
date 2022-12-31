9d81c4e27016f4238de9b73a3596ced87f792ea4
Mahmoud Khalil <m94khalil@gmail.com>
2022-12-31 18:51:49 +0400

    modefied atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: mkhalil <mkhalil@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/27 17:52:57 by mkhalil           #+#    #+#             */
-/*   Updated: 2022/07/04 14:37:54 by mkhalil          ###   ########.fr       */
+/*   Updated: 2022/12/31 18:50:46 by mkhalil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 int	ft_atoi(const char *str)
 {
-	int	sign;
-	int	num;
+	int						sign;
+	unsigned long long int	num;
 
 	sign = 1;
 	num = 0;
@@ -26,10 +26,14 @@ int	ft_atoi(const char *str)
 		if (*str++ == '-')
 			sign *= -1;
 	while (*str >= '0' && *str <= '9')
+	{
 		num = num * 10 + (*str++ - 48);
-	if ((unsigned int)num > 2147483647 && sign == 1)
+		if (num > 9223372036854775807)
+			break ;
+	}
+	if (num >= 9223372036854775807 && sign == 1)
 		return (-1);
-	if ((unsigned int)num > 2147483648 && sign == -1)
+	if (num > 9223372036854775807 && sign == -1)
 		return (0);
-	return (num * sign);
+	return ((int)(num * sign));
 }
